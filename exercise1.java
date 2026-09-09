42e2cc36681938831b4148a6432b06b69450de1f
Vincent Neil M. Ditan <ditanvincentneil5@gmail.com>
2026-09-09 08:54:18 +0800

    Exercise 1

---
+++ exercise1.java
@@ -0,0 +1,19 @@
+
+public class exercise1{
+
+    public static void main(String args[]){
+		int k = 123;
+		int e = 2021;
+		int n = 13;
+		
+		int average = (k + e + n)/3;
+		
+		System.out.println(average);
+	
+
+        
+    }
+}
+
+
+
