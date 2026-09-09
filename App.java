ace2c6c3ddf3f4f0d91a203f8c5ac1a53a9b8b24
Vincent Neil M. Ditan <ditanvincentneil5@gmail.com>
2026-09-09 10:54:21 +0800

    ditan-exercise2.java
    ditan-exercise2.java

---
+++ App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args){
+        int A = 2021, B = 10, C = 50;
+        int Greater = (A > B && A >) ? A : (B > C ? B : C);
+        System.out.print(Greater);
+    }
+}
