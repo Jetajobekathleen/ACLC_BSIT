4c6ff64ffcd396c3a200b9a78074a012e0a0698a
kcassandrabuenaobra <kcassandrabuenaobra@gmail.com>
2026-09-09 08:25:54 +0800

    excercise1
    oscillada exercise 1

---
+++ exercise1.java
@@ -0,0 +1,13 @@
+public class exercise1 {
+   
+    public static void main(String[] args) {
+           
+            int a = 123;
+            int b = 2021;
+            int c = 13;
+
+            int average = (a + b + c) / 3;
+
+            System. out.println("Average: " + average);
+    }
+}
