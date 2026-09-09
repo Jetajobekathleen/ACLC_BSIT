ca34bc47dbf7304334df30a409e0bbce3642284b
narvadezvanessa <narvadezvanessa1@gmail.com>
2026-09-09 08:53:56 +0800

    Add files via upload

---
+++ narvadez javatest/narvadez javatest/src/code.java
@@ -0,0 +1,13 @@
+public class code {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+    public static void main (String[] args) {
+
+        int num1 = 123;
+        int num2 = 2021;
+        int num3 = 13;
+
+        double average = (num1 + num2 + num3)/3.0;
+
+        System.out.println("Average:"+
+        average);
+    }
+    }
