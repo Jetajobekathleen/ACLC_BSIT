2bff32223717b1eba31c42dc03dff4e6e0d38b1c
narvadezvanessa <narvadezvanessa1@gmail.com>
2026-09-09 10:52:29 +0800

    Add files via upload

---
+++ exercise2.java
@@ -0,0 +1,9 @@
+public class exercise2 {
+    public static void main(String[]args) {
+        int v = 10;
+        int a = 50;
+        int n = 2021;
+        int Vanessa = (n > a) ? ((n>v)? n : v) : ((v>n) ? n : a);
+        System.out.println(Vanessa);
+}
+}
