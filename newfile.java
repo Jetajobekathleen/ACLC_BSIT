60ab2a6637b51f0cb3667623986c3116356dbb13
rheanchavez018 <rheanchavez018@gmail.com>
2026-09-10 08:42:17 +0800

    Excercise1
    Chavez, excercise1

---
+++ newfile.java
@@ -0,0 +1,16 @@
+public class Main {
+
+		public static void main(String[] args) {
+		  System.out.println () ;
+		  
+		   int num1=123;
+		   int num2=2021;
+		   int num3=13;
+		   
+		   int average = (num1 + num2 + num3)  /  3;
+		   
+		   System.out.println ( "Average: " + average) ; 
+		  
+	}
+	
+}
